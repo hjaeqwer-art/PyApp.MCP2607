cbd1aaa60dbbd92ec54a7f00a0a04c549b614072
hjaeqwer-art <hjaeqwer@gmail.com>
2026-08-11 21:10:48 +0900

    로또번호 생성기능 추가

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import os, time, random
 st.title('Hello Streamlit')
 st.write('안녕하세요. 배포 참 쉽죠?')
+st.write(time.strtime('%Y-%m-%d %H:%M:%S'))
+
+st.title(':sparkles: 로또 생성기 :sparkles:')
+
+def generate_info():
+  lotto = [i + 1 for i in range(45)]
+  random.shuffle(lotto)
+  return lotto[:6]
+
+button = st.button('로또를 생성해주세요')
+
+if button:
+  for i in range(5):
+    st.subheader(f'{i + 1} 행운의 번호: :green[{generate_lotto()}]')
